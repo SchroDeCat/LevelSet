80e5d1f63c5c821905c2c5bf5a74ba53d4c6b14f
Fengxue Zhang <zhangfx7@mail2.sysu.edu.cn>
2018-05-16 21:26:57 +0800

    correct the iteration by adding weights of area

--- EVOL_CV.m
+++ EVOL_CV.m
@@ -1,29 +1,29 @@
-function [phi, C1, C2] = EVOL_CV(phi0,Img,lambda1,lambda2,mu,pu,timestep,epsilon,numIter)  
-%   input:  
-%       Img: input image  
-%       phi0: level set function to be updated  
-%       mu: weight for length term  
-%       nu: weight for area term, default value 0  
-%       lambda_1:  weight for c1 fitting term  
-%       lambda_2:  weight for c2 fitting term  
-%       timestep: time step  
-%       epsilon: parameter for computing smooth Heaviside and dirac function  
-%       numIter: number of iterations  
-%   output:  
-%       phi: updated level set function  
-% Author: HSW  
-% Date  : 2014/4/8  
-% Harbin institute of technology  
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  
+function [phi, C1, C2] = EVOL_CV(phi0,Img,lambda1,lambda2,mu,pu,timestep,epsilon,numIter)
+%   input:
+%       Img: input image
+%       phi0: level set function to be updated
+%       mu: weight for length term
+%       nu: weight for area term, default value 0
+%       lambda_1:  weight for c1 fitting term
+%       lambda_2:  weight for c2 fitting term
+%       timestep: time step
+%       epsilon: parameter for computing smooth Heaviside and dirac function
+%       numIter: number of iterations
+%   output:
+%       phi: updated level set function
+% Author: HSW
+% Date  : 2014/4/8
+% Harbin institute of technology
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 phi = phi0;
-for k=1:numIter  
-    phi = NeumannBoundCond(phi);  
-    delta_h = Dirac(phi,epsilon);  
-    H = Heaviside(phi,epsilon);  
-    Curv = curvature(phi,2);  
-    [C1,C2]=binaryfit_CV(Img,H); 
-    F_CV = (-lambda1*(Img-C1).^2+lambda2*(Img-C2).^2);  
-    penalizingTerm=pu*(4*del2(phi)-Curv);  
-    lengthTerm = delta_h.*Curv ;  
-    phi=phi+timestep*(delta_h.* F_CV+ mu.*lengthTerm+penalizingTerm);  
-end  
+for k=1:numIter
+    phi = NeumannBoundCond(phi);
+    delta_h = Dirac(phi,epsilon);
+    H = Heaviside(phi,epsilon);
+    Curv = curvature(phi,2);
+    [C1,C2]=binaryfit_CV(Img,H);
+    F_CV = (-lambda1*(Img-C1).^2+lambda2*(Img-C2).^2);
+    penalizingTerm=pu*(4*del2(phi)-Curv);
+    lengthTerm = delta_h.*Curv ;
+    phi=phi+timestep*(delta_h.* (F_CV - nu)+ mu.*lengthTerm+penalizingTerm );  
+end
